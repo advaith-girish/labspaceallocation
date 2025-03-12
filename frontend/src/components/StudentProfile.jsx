30919a56ba656018f59be3b10c0432c398963438
Prithvi <bhatprithviganapathi@gmail.com>
2025-03-13 00:55:59 +0530

    changed the profile page design

--- frontend/src/components/StudentProfile.jsx
+++ frontend/src/components/StudentProfile.jsx
@@ -4,28 +4,28 @@ import "./StudentProfile.css";
 function StudentProfile() {
   return (
     <div className="container">
-    
       <div className="content">
-        <h1 className="pageTitle" >Student's Profile</h1>
+        <h1 className="pageTitle">STUDENT PROFILE</h1>
 
-        <div className="profileContainer">
-       
-          <div className="allocationBox">
-            <h3>Current Allocation:</h3>
+        <div className="profileWrapper">
+          {/* Allocation Card */}
+          <div className="card allocationBox">
+            <h3>Current Allocation</h3>
             <p><b>Lab:</b> BDL</p>
             <p><b>Seat No.:</b> B1</p>
             <p><b>Type:</b> Desktop-based</p>
-            <h3>Past Allocations:</h3>
+            <h3>Past Allocations</h3>
             <p>...</p>
             <p>...</p>
             <p>...</p>
           </div>
 
-        
-          <div className="detailsBox">
-            <p><b>Name:</b> Student</p>
-            <p><b>Email:</b> YYY</p>
-            <p><b>Roll Number:</b> YYY</p>
+          {}
+          <div className="card detailsBox">
+            <h3>Student Details</h3>
+            <p><b>Name:</b> Arjun Sarath</p>
+            <p><b>Email:</b> as@example.com</p>
+            <p><b>Roll Number:</b> 12345</p>
           </div>
         </div>
       </div>
