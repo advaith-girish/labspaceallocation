3d0d5b4b0ac5f54ebc1eeed918aeaffe2c5ed935
Adithyan3001 <adhithyan3124@gmail.com>
2025-03-26 00:02:35 +0530

    server_user_database

---
+++ labspace/src/main/java/com/selab/labspace/repository/ServerUserRepository.java
@@ -0,0 +1,12 @@
+package com.selab.labspace.repository;
+
+import com.selab.labspace.model.ServerUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ServerUserRepository extends JpaRepository<ServerUser, Long> {
+    List<ServerUser> findByLabId(int labId);
+}
